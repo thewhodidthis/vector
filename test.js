33488f9f4025345101d87d4fb6e4bbce3259fb7a
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-18 22:50:15 +0000

    test: tweak for tapeless latest

--- test.js
+++ test.js
@@ -5,24 +5,33 @@ const { createVector } = require('./')
 
 const { x, y } = createVector(2)
 
-equal(x, 2, 'x is a match', 'will default')
-equal(y, 0, 'y is a match')
+equal
+  .describe('x is a match')
+  .test(x, 2)
+  .describe('y is a match', 'will default')
+  .test(y, 0)
 
 const v1 = createVector(2)
 const v2 = createVector(2)
 const v3 = v1.add(v2)
 
-equal(v3.x, 4, undefined, 'will add')
-equal(v3.y, 0)
+equal
+  .test(v3.x, 4)
+  .describe(undefined, 'will add')
+  .test(v3.y, 0)
 
 const v4 = createVector(1, 1)
 const v5 = v4.invert()
 
-equal(v5.x, -1, undefined, 'will invert')
-equal(v5.y, -1)
+equal
+  .test(v5.x, -1)
+  .describe(undefined, 'will invert')
+  .test(v5.y, -1)
 
 const v6 = createVector(1, 1)
 const v7 = v6.multiply(v6)
 const v8 = v7.length()
 
-equal(v8, Math.sqrt(v7.x + v7.y), undefined, 'will operate')
+equal
+  .describe(undefined, 'will operate')
+  .test(v8, Math.sqrt(v7.x + v7.y))
